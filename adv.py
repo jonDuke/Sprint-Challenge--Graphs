fe9ddf0b695d84ce0c6d262d54930ceb7aec3212
jonDuke <jonduke90@gmail.com>
2020-08-13 13:40:37 -0700

    New solution, chooses next room to move to in a smart way, lowest score so far (929)

--- adv.py
+++ adv.py
@@ -26,6 +26,14 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+# Overall pseudocode:
+
+# Build traversal graph
+# Start moving the player.  At each move:
+    # Find all rooms we haven't visited yet that are on the edge of the visited graph
+    # Count the number of unvisited rooms beyond each of those
+    # Move to the edge room with the lowest number of unvisited connected rooms
+
 def build_graph(current_room, graph):
     """ recursive depth first traversal, builds the traversal_graph """
     # Add current_room to the graph
@@ -49,40 +57,65 @@ def build_graph(current_room, graph):
 # keys are the room id's, values are a dict of connected rooms and the directions they are in
 traversal_graph = build_graph(player.current_room, {})
 
-print(traversal_graph)
-
-def find_closest_unvisited(start_room, visited):
+def find_edge_paths(start_room, visited):
     """
-    performs a breath-first search
-    returns the path to the closest unvisited room as a list of room id's
+    Performs a breath-first search, stopping at each unvisited room.
+    Returns a list of paths to each of those unvisited edge rooms.
+    Each path is a list of room id's.
     """
     # Set up the queue and a visited set for this search
     v = set()
     q = deque()
     q.append([start_room])  # queue contains path lists
+    edge_paths = []
 
     while len(q) > 0:
         path = q.popleft()
         v.add(path[-1]) # mark this room as visited for the bfs
         
-        # if this room isn't in visited, return the path to it
+        # if this room isn't in visited, add the path to it
         if path[-1] not in visited:
-            return path
-
+            edge_paths.append(path)
         # else, add neighbors to the queue
-        for room in traversal_graph[path[-1]].keys():
-            if room not in v:
-                new_path = path.copy()
-                new_path.append(room)
-                q.append(new_path)
+        else:
+            for room in traversal_graph[path[-1]].keys():
+                if room not in v:
+                    new_path = path.copy()
+                    new_path.append(room)
+                    q.append(new_path)
+    
+    return edge_paths
+
+def count_unvisited(start_room, visited, v=None):
+    """ recursive dft, counts number of unvisited rooms """
+    # visited set for just this dft
+    if v is None:
+        v = set()    
+    v.add(start_room)
+    
+    count = 1  # count this room
+    for room in traversal_graph[start_room].keys():  # for each neighbor...
+        # if that neighbor is in neither visited or v...
+        if (room not in visited) and (room not in v):
+            # count the rooms beyond those ones
+            count += count_unvisited(room, visited, v)
+    return count
 
 # Fill traversal_path
 traversal_path = []
 visited = set()
 visited.add(player.current_room.id)
 while len(visited) < len(traversal_graph): # loop until we've visited every room
-    # Find the path to the closest unvisited room
-    next_path = find_closest_unvisited(player.current_room.id, visited)
+    # Find paths to each unvisited edge room
+    edge_paths = find_edge_paths(player.current_room.id, visited)
+
+    # Count number of rooms beyond each edge room, saving the path with the lowest count
+    next_path = edge_paths[0]
+    lowest = count_unvisited(next_path[-1], visited)
+    for path in edge_paths[1:]:
+        count = count_unvisited(path[-1], visited)
+        if count < lowest:
+            next_path = path
 
     # Move along that path (first room in next_path is the room we're already in)
     for room in next_path[1:]:
